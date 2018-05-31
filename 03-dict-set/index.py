b581a1672200789929ecdabb72397a5f5367743a
Risto Hinno <ristohinno@gmail.com>
2018-05-31 21:56:49 +0300

    added data

--- 03-dict-set/index.py
+++ 03-dict-set/index.py
@@ -7,11 +7,12 @@
 
 import sys
 import re
-
+import os
+print(os.getcwd())
 WORD_RE = re.compile('\w+')
 
 index = {}
-with open(sys.argv[1], encoding='utf-8') as fp:
+with open(os.path.join(os.getcwd(),"data","text"), encoding='utf-8') as fp:
     for line_no, line in enumerate(fp, 1):
         for match in WORD_RE.finditer(line):
             word = match.group()
